2a33fd55ea1bb994def81dba86aac9e596901d97
venturdan <venturdan@outlook.com.br>
2023-09-19 01:47:32 -0300

    reqs

---
+++ src/migrations/20230918132518-posts_categories.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      postId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        field: 'post_id',
+        references: {
+          model: 'blog_posts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      categoryId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
+  },
+
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
